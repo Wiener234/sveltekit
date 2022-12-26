d85257246f44d781c57f0e978bdbc2236444ca0e
Wiener234 <wiemernils@gmail.com>
2022-12-26 21:51:38 +0100

    '4'

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		// target: '#svelte',
+		adapter: adapter(),
 	},
 	preprocess: vitePreprocess(),
 	paths:{
