d1bc2cbf87c3b7c0478cc04539813bddc8db2ef5
Wiener234 <wiemernils@gmail.com>
2023-03-02 00:25:50 +0100

    header mobilde friendly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
     import "../app.css"
-    import {Button, Dropdown, DropdownItem, Chevron} from 'flowbite-svelte'
+    //import {Button, Dropdown, DropdownItem, Chevron} from 'flowbite-svelte'
     import {base} from '$app/paths'
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Dropdown, DropdownItem, Chevron, Button } from 'flowbite-svelte'
     
 </script>
    
@@ -9,22 +10,59 @@
  
 <header class="sticky bg-green-700 top-0 drop-shadow-xl dark:drop-shadow-[0_20px_35px_rgba(255,255,255,0.03)]">
    
-    <nav class="flex flex-row mx-72 text-white dark:text-white">
+<!--    <nav class="flex flex-row max-w-max mx-auto text-white dark:text-white">
    
-<!--logo and name -->
-     <div class="items-end font-bold text-xl flex flex-row p-5 font-serif self-center">
-       <div class="rounded-full h-14 w-14 p-5 bg-gray-500 self-center bg-center bg-cover bg-[url('https://www.fussball.de/export.media/-/action/getLogo/format/2/id/00ES8GNBJO0000A0VV0AG08LVUPGND5I')]"></div>
-       <a class="text-3xl p-5" href="{base}/"> SV Eintrach Reichenwalde </a>
-     </div>
-   
-<!--fold Hamburger-->
-     <!-- <div id="nav-icon" class="h-16 w-16 p-2">
-         <svg xmlns="<http://www.w3.org/2000/svg>" viewBox="0 0 20 20" fill="currentColor">
-           <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
-         </svg>
+<!--logo and name 
+     <div class="items-end font-bold text-sm flex flex-row p-5 font-serif self-center">
+       <div class="rounded-full h-14 w-14 p-5 bg-gray-500 self-center bg-center bg-cover bg-[url('
+       https://www.fussball.de/export.media/-/action/getLogo/format/2/id/00ES8GNBJO0000A0VV0AG08LVUPGND5I
+       ')]"/>
+       <a class="text-sm p-5 md:text-red-500" href="{base}/"> SV Eintrach Reichenwalde </a>
      </div> -->
-     
-        <div class="items-end flex-grow p-5 list-none self-center text-xl font-semibold">
+
+
+<Navbar let:hidden let:toggle color="bg-green-700" navDivClass="flex text-white text-xl font-bold items-center justify-between mx-auto min-md:flex-row max-md:flex-wrap">
+  <NavBrand href="{base}/">
+    <img
+      src="https://www.fussball.de/export.media/-/action/getLogo/format/2/id/00ES8GNBJO0000A0VV0AG08LVUPGND5I"
+      class="mr-3 h-11 sm:h-14 m-3 rounded-full"
+      alt="SV Eintracht Reichenwalde Logo"
+    />
+    <span class="self-center whitespace-normal sm:text-3xl text-sm font-semibold dark:text-white">
+      SV Eintracht Reichenwalde
+    </span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} class="bg-green-700 text-white hover:border-0 fill:border-0 focus:ring-green-700" shadow="null"/>
+  <NavUl {hidden} ulClass="text-white bg-green-700 border-0 flex flex-col md:flex-row ul self-center p-5 md:items-end">
+    <NavLi href="{base}/news" nonActiveClass="hover:underline md:mx-4">News</NavLi>
+    <Button id="nav-menu" btnClass="hover:underline md:mx-4 flex items-center justify-between"><Chevron>Manschaften</Chevron></Button>
+    <NavLi href="{base}/kontakt" nonActiveClass="hover:underline md:mx-4">Kontakt</NavLi>
+    <Dropdown triggeredBy="#nav-menu" class="ul w-44 z-20">
+      <DropdownItem><a href="{base}/manschaften/c-jugend">C-Jugend</a></DropdownItem>
+      <DropdownItem><a href="{base}/manschaften/d-jugend">D-Jugend</a></DropdownItem>
+      <DropdownItem><a href="{base}/manschaften/e-jugend">E-Jugend</a></DropdownItem>
+    </Dropdown>
+
+  </NavUl>
+</Navbar>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--        <div class="items-end flex-grow p-5 list-none self-center text-xl font-semibold hidden">
             <ul class="list-none ul flex flex-row-reverse">
               <li class="mx-4 order-last  hover:underline"><a href="{base}/news">News</a></li>
               <li class="mx-4 hover:underline">
@@ -42,10 +80,10 @@
             </ul>
         </div>
 
-    </nav>
+    </nav>-->
 </header>
 
-   <main class="text-black dark:text-white dark:bg-gray-800  min-h-full mx-72 my-10">
+   <main class="text-black dark:text-white dark:bg-gray-800  min-h-full max-w-4xl mx-auto my-10">
     <slot></slot>
    </main>
 
